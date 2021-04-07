eaa56d981c8499a196c807104907a1a11b0e0547
VYOMTAMTA <77063970+VYOMTAMTA@users.noreply.github.com>
2021-04-07 20:25:59 +0530

    Add files via upload

---
+++ drop.js
@@ -0,0 +1,33 @@
+class Drop{
+
+    constructor(x, y){
+    
+        var options = {
+            isStatic: false,
+            restitution: 0.2,
+        }
+        this.body = Bodies.circle(x, y, 5, options);
+        this.radius=5;
+        World.add(world, this.body);
+        
+    }
+    
+    update(){
+    
+      if(this.body.position.y > height){
+          Matter.Body.setPosition(this.body, {x:random(0, 500), y:(0, 30)})
+      }
+    
+    }
+    
+    display(){
+    
+     noStroke();
+     fill("blue");
+     ellipseMode(CENTER);
+     ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius);
+    
+    }
+    
+    
+    }
